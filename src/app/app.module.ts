e747e380f6a20521cde26f7b344d9387616e3fd8
Dan Moore <github@mooreds.com>
2020-03-23 23:14:46 +0000

    add missing routing pieces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -10,6 +11,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
