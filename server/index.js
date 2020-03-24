6c909b33c8fe90b314dbcc3d1f8bf10f80547d23
Dan Moore <github@mooreds.com>
2020-03-24 15:04:08 +0000

    remove session middleware, not needed at this time

--- server/index.js
+++ server/index.js
@@ -11,20 +11,6 @@ app.use(cors({
   credentials: true
 }));
 
-// configure sessions
-app.use(session(
-  {
-    secret: '1234567890',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: 'auto',
-      httpOnly: true,
-      maxAge: 3600000
-    }
-  })
-);
-
 app.use('/user', require('./routes/user'));
 
 app.listen(config.serverPort, () => console.log(`FusionAuth example app listening on port ${config.serverPort}.`));
