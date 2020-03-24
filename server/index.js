7cf174f8b181f82ae30ff99ca6b3c4417596ebb0
Dan Moore <github@mooreds.com>
2020-03-24 16:29:38 +0000

    add set user data route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const config = require('./config');
 
 const app = express();
+
+// install the JSON middleware for parsing JSON bodies
 app.use(express.json());
 
 app.use(cors({
@@ -30,6 +32,7 @@ app.use('/user', require('./routes/user'));
 app.use('/login', require('./routes/login'));
 app.use('/oauth-callback', require('./routes/oauth-callback'));
 app.use('/logout', require('./routes/logout'));
+app.use('/set-user-data', require('./routes/set-user-data'));
 
 app.listen(config.serverPort, () => console.log(`FusionAuth example app listening on port ${config.serverPort}.`));
 
---
+++ server/routes/set-user-data.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../config');
+
+router.post('/', (req, res) => {
+  // fetch the user using the token in the session so that we have their ID
+  request(
+    // POST request to /introspect endpoint
+    {
+      method: 'POST',
+      uri: `http://localhost:${config.fusionAuthPort}/oauth2/introspect`,
+      form: {
+        'client_id': config.clientID,
+        'token': req.session.token
+      }
+    },
+
+    // callback
+    (error, response, body) => {
+      let introspectResponse = JSON.parse(body);
+      request(
+        // PATCH request to /registration endpoint
+        {
+          method: 'PATCH',
+          uri: `http://localhost:${config.fusionAuthPort}/api/user/registration/${introspectResponse.sub}/${config.applicationID}`,
+          headers: {
+            'Authorization': config.apiKey
+          },
+          json: true,
+          body: {
+            'registration': {
+              'data': req.body
+            }
+          }
+        },
+        (err2, response2, body2) => {
+          console.log(err2);
+        }
+      );
+    }
+  );
+});
+
+module.exports = router;
+
