6f95a736f640b104b58a8f965eacb73248ff8a8d
Dan Moore <github@mooreds.com>
2020-03-24 02:05:34 +0000

    add injection of user service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  user = {}
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.get().subscribe((data: any)=>{  
+      console.log(data);  
+      this.user = data;  
+    });
   }
 
 }
