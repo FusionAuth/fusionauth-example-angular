75d1db2de326bb415a95dd2c8069730d0fc74e4a
Dan Moore <github@mooreds.com>
2020-03-24 16:58:02 +0000

    adding forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { UserDataFormComponent } from './user-data-form/user-data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserDataFormComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule, 
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
