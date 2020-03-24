215ea2426988f3be05db23fcf1d7ccf535fa4041
Dan Moore <github@mooreds.com>
2020-03-24 15:27:19 +0000

    add proxying login route

---
+++ server/routes/login.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config');
+
+router.get('/', (req, res) => {
+  res.redirect(`http://localhost:${config.fusionAuthPort}/oauth2/authorize?client_id=${config.clientID}&redirect_uri=${config.redirectURI}&response_type=code`);
+});
+
+module.exports = router;
+
+
