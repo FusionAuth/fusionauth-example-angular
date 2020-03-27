4e9192741724d7f468d28a4c7f19504a5f527da4
Dan Moore <github@mooreds.com>
2020-03-27 18:44:18 +0000

    better handling of logging and change to use userinfo endpoint

--- server/routes/set-user-data.js
+++ server/routes/set-user-data.js
@@ -6,24 +6,22 @@ const config = require('../config');
 router.post('/', (req, res) => {
   // fetch the user using the token in the session so that we have their ID
   request(
-    // POST request to /introspect endpoint
     {
-      method: 'POST',
-      uri: `http://localhost:${config.fusionAuthPort}/oauth2/introspect`,
-      form: {
-        'client_id': config.clientID,
-        'token': req.session.token
+      method: 'GET',
+      uri: `http://localhost:${config.fusionAuthPort}/oauth2/userinfo`,
+      headers: {
+        'Authorization': 'Bearer ' + req.session.token
       }
     },
 
     // callback
     (error, response, body) => {
-      let introspectResponse = JSON.parse(body);
+      let userInfoResponse = JSON.parse(body);
       request(
         // PATCH request to /registration endpoint
         {
           method: 'PATCH',
-          uri: `http://localhost:${config.fusionAuthPort}/api/user/registration/${introspectResponse.sub}/${config.applicationID}`,
+          uri: `http://localhost:${config.fusionAuthPort}/api/user/registration/${userInfoResponse.sub}/${config.applicationID}`,
           headers: {
             'Authorization': config.apiKey
           },
@@ -35,7 +33,9 @@ router.post('/', (req, res) => {
           }
         },
         (err2, response2, body2) => {
-          console.log(err2);
+          if (err2) {
+            console.log(err2);
+          }
         }
       );
     }
